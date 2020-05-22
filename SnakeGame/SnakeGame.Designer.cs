3964b188c25c3f6f1add3f7228531712d774ae1f
Deign77 <david.wesseling1@gmail.com>
2020-05-22 15:07:47 +0100

    Added newgame method and some settings variables

--- SnakeGame/SnakeGame.Designer.cs
+++ SnakeGame/SnakeGame.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SnakeGame
 {
-    partial class Form1
+    partial class SnakeGame
     {
         /// <summary>
         /// Required designer variable.
@@ -34,14 +34,22 @@
             this.lblScoreTotal = new System.Windows.Forms.Label();
             this.lblHiScoreTotal = new System.Windows.Forms.Label();
             this.lblGameOver = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblBonusFoodTimer = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbGameBoard)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pbGameBoard
             // 
-            this.pbGameBoard.Location = new System.Drawing.Point(228, 181);
+            this.pbGameBoard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pbGameBoard.Location = new System.Drawing.Point(0, 24);
             this.pbGameBoard.Name = "pbGameBoard";
-            this.pbGameBoard.Size = new System.Drawing.Size(100, 50);
+            this.pbGameBoard.Size = new System.Drawing.Size(626, 622);
             this.pbGameBoard.TabIndex = 0;
             this.pbGameBoard.TabStop = false;
             // 
@@ -49,7 +57,7 @@
             // 
             this.lblScore.AutoSize = true;
             this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblScore.Location = new System.Drawing.Point(149, 703);
+            this.lblScore.Location = new System.Drawing.Point(57, 725);
             this.lblScore.Name = "lblScore";
             this.lblScore.Size = new System.Drawing.Size(65, 24);
             this.lblScore.TabIndex = 1;
@@ -59,7 +67,7 @@
             // 
             this.lblHiScore.AutoSize = true;
             this.lblHiScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHiScore.Location = new System.Drawing.Point(328, 703);
+            this.lblHiScore.Location = new System.Drawing.Point(434, 725);
             this.lblHiScore.Name = "lblHiScore";
             this.lblHiScore.Size = new System.Drawing.Size(110, 24);
             this.lblHiScore.TabIndex = 2;
@@ -69,7 +77,7 @@
             // 
             this.lblScoreTotal.AutoSize = true;
             this.lblScoreTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblScoreTotal.Location = new System.Drawing.Point(220, 703);
+            this.lblScoreTotal.Location = new System.Drawing.Point(128, 725);
             this.lblScoreTotal.Name = "lblScoreTotal";
             this.lblScoreTotal.Size = new System.Drawing.Size(20, 24);
             this.lblScoreTotal.TabIndex = 3;
@@ -79,7 +87,7 @@
             // 
             this.lblHiScoreTotal.AutoSize = true;
             this.lblHiScoreTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHiScoreTotal.Location = new System.Drawing.Point(444, 703);
+            this.lblHiScoreTotal.Location = new System.Drawing.Point(550, 725);
             this.lblHiScoreTotal.Name = "lblHiScoreTotal";
             this.lblHiScoreTotal.Size = new System.Drawing.Size(20, 24);
             this.lblHiScoreTotal.TabIndex = 4;
@@ -89,26 +97,81 @@
             // 
             this.lblGameOver.AutoSize = true;
             this.lblGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGameOver.Location = new System.Drawing.Point(286, 545);
+            this.lblGameOver.Location = new System.Drawing.Point(262, 640);
             this.lblGameOver.Name = "lblGameOver";
             this.lblGameOver.Size = new System.Drawing.Size(50, 25);
             this.lblGameOver.TabIndex = 5;
             this.lblGameOver.Text = "G O";
             // 
-            // Form1
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(626, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // lblBonusFoodTimer
+            // 
+            this.lblBonusFoodTimer.AutoSize = true;
+            this.lblBonusFoodTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBonusFoodTimer.Location = new System.Drawing.Point(292, 699);
+            this.lblBonusFoodTimer.Name = "lblBonusFoodTimer";
+            this.lblBonusFoodTimer.Size = new System.Drawing.Size(20, 24);
+            this.lblBonusFoodTimer.TabIndex = 7;
+            this.lblBonusFoodTimer.Text = "0";
+            // 
+            // SnakeGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(637, 774);
+            this.ClientSize = new System.Drawing.Size(626, 785);
+            this.Controls.Add(this.lblBonusFoodTimer);
             this.Controls.Add(this.lblGameOver);
             this.Controls.Add(this.lblHiScoreTotal);
             this.Controls.Add(this.lblScoreTotal);
             this.Controls.Add(this.lblHiScore);
             this.Controls.Add(this.lblScore);
             this.Controls.Add(this.pbGameBoard);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SnakeGame";
+            this.Text = "SnakeGame";
             ((System.ComponentModel.ISupportInitialize)(this.pbGameBoard)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +185,12 @@
         private System.Windows.Forms.Label lblScoreTotal;
         private System.Windows.Forms.Label lblHiScoreTotal;
         private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.Label lblBonusFoodTimer;
     }
 }
 
